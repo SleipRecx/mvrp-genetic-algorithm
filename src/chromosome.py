2ef46b3646ca2b6e36addc7703b8ae3c5b1e6808
Markus Andresen <markus.andresen94@gmail.com>
2018-02-07 14:23:09 +0100

    basic chromosome functionality

--- src/chromosome.py
+++ src/chromosome.py
@@ -1,26 +1,68 @@
 import numpy as np
+import random
+from copy import deepcopy
+from collections import defaultdict
 from typing import Dict
+from pprint import pprint
+
+from src.util import read_problem_file
 
 
 class Chromosome:
-    def __init__(self, problem):
-        self.customers = problem.customers
-        self.depots = problem.depots
-        self.routes = {}
-        self.initial_cluster = self.get_initial_cluster()
-        print(self.initial_cluster)
-        for depot in self.depots:
-            self.routes[depot[0]] = []
+    def __init__(self, customers, depots, max_vehicles):
+        self.customers = customers
+        self.depots = depots
+        self.max_vehicles = max_vehicles
+        self.routes = self.generate_random_routes()
 
-    def get_initial_cluster(self) -> Dict:
-        cluster = {}
-        for customer in self.customers:
+    def get_customer_cluster(self) -> Dict:
+        cluster = defaultdict(list)
+        for customer_id in self.customers:
+            customer_coordinate = self.customers[customer_id][0]
             best_distance = float('inf')
             best_depot = None
-            for depot in self.depots:
-                distance = np.linalg.norm(customer[1:3] - depot[1:3])
+            for depot_id in self.depots:
+                depot_coordinate = self.depots[depot_id][0]
+                distance = np.linalg.norm(customer_coordinate - depot_coordinate)
                 if distance < best_distance:
-                    best_depot = depot[0]
+                    best_depot = depot_id
                     best_distance = distance
-            cluster[customer[0]] = best_depot
+            cluster[best_depot].append(customer_id)
         return cluster
+
+    # TODO: add cars without customers to route (maybe)
+    def generate_random_routes(self) -> Dict:
+        routes = defaultdict(list)
+        cluster = self.get_customer_cluster()
+        empty_route = [[] for _ in range(self.max_vehicles)]
+        for depot_id, customers in cluster.items():
+            routes[depot_id] = deepcopy(empty_route)
+            for customer_id in customers:
+                route_number = random.randint(0, self.max_vehicles - 1)
+                routes[depot_id][route_number].append(customer_id)
+        return routes
+
+    def calculate_fitness(self):
+        distance = 0
+        load_exceeded_count = 0
+        for depot_id, routes in self.routes.items():
+            depot_coordinate = self.depots[depot_id][0]
+            for route in routes:
+                demand = sum(list(map(lambda x: self.customers[x][2], route)))
+                if demand > self.depots[depot_id][2]:
+                    load_exceeded_count += 1
+                trip = list(map(lambda x: self.customers[x][0], route))
+                trip.append(depot_coordinate)
+                trip.insert(0, depot_coordinate)
+                for i in range(len(trip) - 1):
+                    distance += np.linalg.norm(trip[i] - trip[i + 1])
+        return 1 / (distance + 1 * (load_exceeded_count + 1))
+
+
+if __name__ == '__main__':
+    c, d, m = read_problem_file("../data/problem/p01")
+    solution = Chromosome(c, d, m)
+    print(solution.calculate_fitness())
+
+
+
--- src/util.py
+++ src/util.py
@@ -1,29 +1,40 @@
 from typing import Tuple
+from collections import defaultdict
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
-import numpy as np
-from problem import Problem
 
 
-def parse_problem(filename: str) -> Problem:
+def read_problem_file(filename: str) -> Tuple:
     file = open(filename, "r")
     line1 = list(map(lambda e: int(e), file.readline().split()))
-    number_of_vehicles = line1[0]
+    max_vehicles = line1[0]
     number_of_customers = line1[1]
     number_of_depots = line1[2]
-    depots = []
     tmp_depots = []
-    customers = []
+    depots = defaultdict(list)
+    customers = defaultdict(list)
     for i in range(number_of_depots):
-        tmp_depots.append(list(map(lambda e: int(e), file.readline().split())))
+        line = list(map(lambda e: int(e), file.readline().split()))
+        tmp_depots.append(line)
     for i in range(number_of_customers):
-        customers.append(list(map(lambda e: int(e), file.readline().split()[0:5])))
+        line = list(map(lambda e: int(e), file.readline().split()[0:5]))
+        customer_id = line[0]
+        coordinate = np.array([line[1], line[2]])
+        service_duration = line[3]
+        demand = line[4]
+        customers[customer_id].extend([coordinate, service_duration, demand])
     for i in range(number_of_depots):
-        depots.append(list(map(lambda e: int(e), file.readline().split()[0:3])))
-        depots[i].extend(tmp_depots[i])
-    return Problem(np.array(customers), np.array(depots), number_of_vehicles)
+        line = list(map(lambda e: int(e), file.readline().split()[0:3]))
+        depot_id = line[0]
+        coordinate = np.array([line[1], line[2]])
+        max_duration = tmp_depots[i][0]
+        max_load = tmp_depots[i][1]
+        depots[depot_id].extend([coordinate, max_duration, max_load])
+    return customers, depots, max_vehicles
 
 
+"""
 def plot_problem(data):
     customers = data.customers
     depots = data.depots
@@ -45,9 +56,4 @@ def plot_problem(data):
                         mpatches.Patch(color='red', label=str(len(depots)) + ' Depots'),
                         mpatches.Patch(color='yellow', label=str(number_of_vehicles) + ' Vehicles')])
     plt.show()
-
-
-
-if __name__ == '__main__':
-    problem = parse_problem("../data/problem/p01")
-    plot_problem(problem)
+"""
